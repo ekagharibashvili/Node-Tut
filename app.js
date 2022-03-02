210a2ce8740432c1244d243d77bb8cb72168e9ed
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-03-02 18:13:44 +0400

    Feat

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ const start = async () => {
 
 start();
 
-const getText = (path) => {
+/* const getText = (path) => {
   return new Promise((resolve, reject) => {
     readFile(path, "utf8", (err, data) => {
       if (err) {
@@ -137,4 +137,4 @@ const getText = (path) => {
 
 getText("./content/first.txt")
   .then((result) => console.log(result))
-  .catch((err) => console.log(err)); 
+  .catch((err) => console.log(err));  */
